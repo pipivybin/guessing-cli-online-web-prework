e4e4e6a1441b568d6e8271de8cd103478e649f69
Ming Duan <11203duan@gmail.com>
2019-03-05 18:39:42 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   loop do
-  endputs "Guess a number between 1 and 6."
+  puts "Guess a number between 1 and 6."
   input = gets.strip.chomp
   comp = rand(1..6)
   if input == "exit"
